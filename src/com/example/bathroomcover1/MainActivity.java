70944becf5ab9d4ad1d6695fa3145392c100f191
jjanicki <jajanicki@gmail.com>
2014-08-23 21:36:02 -0500

    Final build before deploying

--- src/com/example/bathroomcover1/MainActivity.java
+++ src/com/example/bathroomcover1/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import com.trogdor.bathroomcover1.R;
 
 public class MainActivity extends Activity {
 
