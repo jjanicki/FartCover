fe2e3a796312e19c3ff6a0c7f6a52c7a55920a93
jjanicki <jajanicki@gmail.com>
2015-01-28 01:10:29 -0600

    Pushing changes for 2.0 release.  Gee, I hope I didn't break anything.

--- src/com/example/bathroomcover1/MainActivity.java
+++ src/com/example/bathroomcover1/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.bathroomcover1;
 import android.app.Activity;
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import com.trogdor.bathroomcover1.R;
+
+import java.io.File;
 
 
 public class MainActivity extends Activity {
@@ -31,12 +34,12 @@ public class MainActivity extends Activity {
 	final Context context = this;
 	private Button button;
 	
-	
 	public void playFaucet(View playFaucet) {
 		if (mp.isPlaying()) {
 			mp.stop();
+            mp.release();
 		}
-		mp = MediaPlayer.create(getApplicationContext(), R.raw.sink3);
+		mp = MediaPlayer.create(getApplicationContext(), R.raw.sink);
 		mp.start();
 		mp.setLooping(true);
 		final Button button = (Button) findViewById(R.id.stopSound);
@@ -50,11 +53,12 @@ public class MainActivity extends Activity {
 	public void playHandDryer(View playHandDryer) {
 		if (mp.isPlaying()) {
 			mp.stop();
+            mp.release();
 		}
 		mp = MediaPlayer.create(getApplicationContext(), R.raw.handdryer);
 		mp.start();
 		mp.setLooping(true);
-				
+
 		final Button button = (Button) findViewById(R.id.stopSound);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View stopSound) {
@@ -62,5 +66,30 @@ public class MainActivity extends Activity {
             }
         });
     }
-		
+
+    public void playToilet(View playToilet) {
+        if (mp.isPlaying()) {
+            mp.stop();
+            mp.release();
+        }
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.toilet);
+        mp.start();
+
+        final Button button = (Button) findViewById(R.id.stopSound);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View stopSound) {mp.stop();} });
+    }
+
+    public void playFart(View playFart) {
+        if (mp.isPlaying()) {
+            mp.stop();
+            mp.release();
+        }
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.fart);
+        mp.start();
+
+        final Button button = (Button) findViewById(R.id.stopSound);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View stopSound) {mp.stop();} });
+    }
 }
